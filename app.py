2ba72040f2780492fb75b8faa983b0dab8e3f2f4
emilianolpz8a <emiliano.lopez8a@gmail.com>
2024-11-05 23:31:56 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ import plotly.express as px
 import streamlit as st
 import pandas as pd
 
-# agregar encabezado de la aplicación
-st.header("Aplicación Web de Visualización de Datos de Vehiculos")
+car_data = pd.read_csv('vehicles_us.csv')
 
-car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
+# agregar encabezado de la aplicación
+st.header("Aplicación web para visualización de datos de vehiculos")
+
+st.dataframe(data = car_data)  # leer los datos
 hist_button = st.button('Construir histograma')  # crear un botón
 
 if hist_button:  # al hacer clic en el botón
@@ -19,9 +21,9 @@ if hist_button:  # al hacer clic en el botón
 
 
 # crear segundo botón para gráfico de dispersión
-disp_button = st.button('Construir gráfico dispersión')
+scatt_button = st.button('Construir gráfico dispersión')
 
-if disp_button:  # al hacer clic en el botón
+if scatt_button:  # al hacer clic en el botón
     # escribe mensaje
     st.write(
         'Creación de una gráfica de dispersión de los anuncios de ventas de coches')
